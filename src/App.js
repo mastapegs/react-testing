cc739b77593466cc6db839712615e0eaa86a00ab
Matthew Pagan <mastapegs01@gmail.com>
2020-12-02 09:24:06 -0500

    initial commit after cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(10)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>To Do App</h1>
+      <p>Value is: {value}</p>
+      <button onClick={() => setValue(value - 1)}>Decrement</button>
+      <button onClick={() => setValue(value + 1)}>Increment</button>
+    </>
+  )
 }
 
 export default App;
